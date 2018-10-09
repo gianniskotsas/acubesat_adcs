1a8f9ee5bae3946b8d0b572f5bed68763d621dae
PoSeiDonTeaM <johnkotsas@me.com>
2018-10-09 17:19:07 +0300

    Change of Control system model

--- important_arduino_project.ino
+++ important_arduino_project.ino
@@ -5,7 +5,8 @@ int valueBig = 0;
 int i=0;
 int aux = 0;
 int aux1 = 0;
-int motor = 10;
+int motorRight = 10;
+int motorLeft = 11;
 
 float error=0;
 
@@ -21,6 +22,15 @@ float ti=0;
 float tf=0;
 float incT=0;
 
+int q=0;
+int q_int=0;
+int q_prev=0;
+int t_prev=0;
+int t_new = 0;
+int dt = 0;
+
+
+
 // Integer
 float ki = 0.2;
 float integral = 0;
@@ -29,96 +39,78 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // Enabling Serial Monitor [top-right]
   pinMode(9, OUTPUT); // DC motor controller
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  
+  valueSmall=analogRead(photoresistorSmall); // get initial values
+  valueBig=analogRead(photoresistorBig);
+  
+  q = valueSmall-valueBig;
+  q_int = q;
+  q_prev = q;
+  t_prev = millis();  
+ 
 }
 
 void loop() {
   int pid_sum = 0;
-  
-  valueSmall = analogRead(photoresistorSmall);
-  valueBig = analogRead(photoresistorBig);
-  //Serial.println(valueSmall);
-  //Serial.println(valueBig);
 
-  error=valueSmall-valueBig; // The error is the difference between the two photoresistors
+  q = getError();
+  t_new = millis();
+
 
   //proportional
-  proportional=k*error;
-  Serial.print("Error is:");
-  Serial.println(proportional);
+  proportional=k*q;
 
   //Integral & Derivative
 
-  derivative=0;
-  //while(i<8){
-    valueSmall=analogRead(photoresistorSmall);
-    valueBig=analogRead(photoresistorBig);
-    aux=valueSmall-valueBig;
+  dt = t_new - t_prev;
+
+  derivative = (q-q_prev)/dt;
+  q_prev = q;
+
+  q_int = q_int + q * dt;
     
-    ti=millis(); // Inserts time passed until this point of the loop
-    
-    Serial.print("ti is:");
-    Serial.println(ti);
-    
-    //print time
-    //Serial.print("Initial time:");
-    //Serial.println(ti);
-    
-    valueSmall=analogRead(photoresistorSmall);
-    valueBig=analogRead(photoresistorBig);
-    aux1=valueSmall-valueBig;
-    
-    tf=millis();
-    
-    Serial.print("tf is:");
-    Serial.println(tf);
-    
-    //print final time
-    //Serial.print("Final time:");
-    //Serial.println(ti);
-    
-    incT=(tf-ti)/1000;   // Calculate in ms
-    
-    derivative=derivative+(aux1-aux)/incT; 
-    integral=integral+(aux1+aux)*incT/2;
-    
-    i++;        
-  //}
+    //incT=(tf-ti)/1000;   // Calculate in ms
+
+  derivative=kd*derivative;
+  integral=ki * q_int;
+
   
-i=0;
 
-derivative=kd*(derivative/8);
-integral=ki*integral;
+  pid_sum = derivative + integral + proportional;
 
-
-Serial.print("Derivative:");
-Serial.println(derivative);
-Serial.print("Integral:");
-Serial.println(integral);
-Serial.print("Proportional:");
-Serial.println(proportional);
-
-pid_sum = derivative + integral + proportional;
-
-Serial.print("PID value =");
-Serial.println(pid_sum);
-
-if(pid_sum > 5)
-  {
-    pid_sum = 5;
+  Serial.print("PID value =");
+  Serial.println(pid_sum);
+  
+  if(pid_sum > 0){
+    
+    if(pid_sum > 5)
+      {
+        pid_sum = 5;
+      }
+      
+      pid_sum = map(pid_sum,0, 5,0, 255);
+      analogWrite(10, pid_sum);
+      analogWrite(11,0);
+  }else{
+  
+  if(pid_sum < -5)
+    {
+      pid_sum = -5;
+    }
+  
+      pid_sum = map(-pid_sum,0, 5,0, 255);
+      analogWrite(11, pid_sum);
+      analogWrite(10, 0);
+      
   }
-
-if(pid_sum < 0)
-  {
-    pid_sum = 0;
-  }
-
-pid_sum = map(pid_sum,0, 5,0, 255);
-
-Serial.print("PWM signal after mapping is:");
-Serial.println(pid_sum);
-
-analogWrite(10, pid_sum);
-Serial.println("----------------------------");
+  
+  Serial.print("PWM signal after mapping is:");
+  Serial.println(pid_sum);
+  
+  //analogWrite(10, pid_sum);
+  Serial.println("----------------------------");
 
 
 /* 
@@ -131,4 +123,14 @@ Serial.println("----------------------------");
  * 
  */
 
+}
+
+int getError(){
+
+  
+  int valueSmall=analogRead(photoresistorSmall);
+  int valueBig=analogRead(photoresistorBig);
+  
+  return valueSmall-valueBig;
+  
 }
